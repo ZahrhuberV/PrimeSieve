08af1d3dbebb9ab77ebb824871123bdd6e5badf4
ZahrhuberV <valentin.zahrhuber@gmx.at>
2022-09-18 15:12:02 +0200

    Interface "PrimeSieve" erstellt und implementiert

---
+++ PrimeSieve/src/com/company/EratosthenesPrimeSieve.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class EratosthenesPrimeSieve implements PrimeSieve{
+
+
+    @Override
+    public boolean isPrime(int p) {
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
+}
---
+++ PrimeSieve/src/com/company/PrimeSieve.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public interface PrimeSieve {
+    public boolean isPrime(int p);
+    public void printPrimes();
+}
