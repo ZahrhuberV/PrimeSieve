c5edcd60fd71ec2cae122f9e1b419cdb30970126
ZahrhuberV <valentin.zahrhuber@gmx.at>
2022-09-18 16:39:45 +0200

    "siev" Methode implementiert

--- PrimeSieve/src/com/company/EratosthenesPrimeSieve.java
+++ PrimeSieve/src/com/company/EratosthenesPrimeSieve.java
@@ -1,7 +1,16 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class EratosthenesPrimeSieve implements PrimeSieve{
 
+    int max;
+    private ArrayList<Boolean> primes = new ArrayList<>();
+
+    public EratosthenesPrimeSieve(int max) {
+        this.max = max;
+        siev();
+    }
 
     @Override
     public boolean isPrime(int p) {
@@ -10,6 +19,25 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
 
     @Override
     public void printPrimes() {
+    }
 
+    public ArrayList<Boolean> siev(){
+
+        for(int i = 0; i <= max; i++) {
+            primes.add(i, true);
+        }
+
+        for(int i = 2; i <= max; i++) {
+            if(primes.get(i) == true) {
+                int j = i;
+                do {
+                    j = j+i;
+                    if(j <= max) {
+                        primes.set(j, false);
+                    }
+                } while(j <= max);
+            }
+        }
+        return primes;
     }
 }
